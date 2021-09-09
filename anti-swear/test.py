3811b01051b859edbbf3860e97e37f68e4a43b17
JT-Potato <joel123droid@outlook.com>
2021-09-09 16:42:35 +1000

    Changed test.py to add timing info

--- anti-swear/test.py
+++ anti-swear/test.py
@@ -1,9 +1,14 @@
 import joblib
-loaded_model = joblib.load("model.joblib")
-vectoriser = joblib.load("vectorizer.joblib")
+import time
+import os
+loaded_model = joblib.load(os.path.dirname(os.path.realpath(__file__)) + "\\model.joblib")
+vectoriser = joblib.load(os.path.dirname(os.path.realpath(__file__)) + "\\vectorizer.joblib")
+
 while True:
     text = input("string: ")
+    pretime = time.time()
     text = [text]
     vText = vectoriser.transform(text)
     prob = loaded_model.predict(vText)
     print(prob)
+    print(f"Calculated in {(time.time() - pretime)*100} ms")
